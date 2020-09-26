4ed0638803f34610ff0a7b8a7ff3f0cf2b1f0cdc
= <=>
2020-09-26 15:23:24 +0700

    finished installing react-navigation-stack

--- tmp.js
+++ tmp.js
@@ -10,14 +10,14 @@ import IAHeader from "./js/shared/components/IAHeader";
 import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
 import {CreditCardInput, LiteCreditCardInput} from "react-native-credit-card-input";
 import * as device from "./js/shared/utils/device/device";
-// import {TextField} from "react-native-material-textfield";
-// import {
-// 	createAppContainer,
-// 	createSwitchNavigator,
-// 	createBottomTabNavigator,
-// } from "react-navigation";
-// import { createStackNavigator } from '@react-navigation/stack';
-// // import { createDrawerNavigator } from '@react-navigation/drawer';
+import {TextField} from "react-native-material-textfield";
+import {
+	createAppContainer,
+	createSwitchNavigator,
+	createBottomTabNavigator,
+} from "react-navigation";
+import {createStackNavigator} from 'react-navigation-stack';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 import PaymentScreen from "./js/containers/PaymentScreen";
 import OptionPaymentVISA from "./js/containers/OptionPaymentVISA";
 import OptionPayment from "./js/containers/OptionPayment";
@@ -27,8 +27,7 @@ export class Screen1 extends BaseScreen{
 	render(){
 		return(
 			<Provider store = {StoreRedux}>
-				<PaymentScreen/>
-
+				<AppContainerTest/>
 			</Provider>
 		);
 	}
@@ -59,7 +58,9 @@ export class AppContainerTest extends React.Component {
 		const MainScreenNavigator = createStackNavigator(
 			{
 				
-				MainScreen: {screen: PaymentScreen},
+				PaymentScreen: PaymentScreen,
+				OptionPaymentVISA: OptionPaymentVISA,
+				OptionPayment: OptionPayment,
 			},
 			{
 				mode: "modal",
@@ -68,108 +69,11 @@ export class AppContainerTest extends React.Component {
 			}
 		);
 		const App = createAppContainer(MainScreenNavigator);
-		return <App ref={navigatorRef => {
-          			NavigationService.setTopLevelNavigator(navigatorRef);
-		}}
-		{...this.props} />;
+		return <App/>;
+		// return <App ref={navigatorRef => {
+        //   			NavigationService.setTopLevelNavigator(navigatorRef);
+		// }}
+		// {...this.props} 
+		// />;
 	}
-
-
 }
-
-const HEADER_HEIGHT = ScreenHeight * 0.3;
-const HEADER_WIDTH = ScreenWidth;
-const styles = StyleSheet.create({
-	mainContainer: {
-		backgroundColor: colors.white,
-		flex: 1,
-	},
-	content: {
-		paddingStart: 27,
-		paddingEnd: 20
-	},
-	drawer: {
-		marginStart: 10
-	},
-	title: {
-		// fontFamily: fonts.family.nunito.regular,
-		fontSize: 22,
-		fontWeight: "normal",
-		fontStyle: "normal",
-		letterSpacing: 0,
-		textAlign: "left",
-		color: colors.black,
-	},
-	request: {
-		// fontFamily: fonts.family.nunito.regular,
-		fontSize: 17,
-		fontWeight: "normal",
-		fontStyle: "normal",
-		letterSpacing: 0,
-		textAlign: "left",
-		color: colors.black,
-	},
-	headerBackground: {
-		backgroundColor: colors.deluge,
-		height: HEADER_HEIGHT,
-		width: HEADER_WIDTH,
-		borderBottomLeftRadius: 60
-	},
-	welcomeTitle: {
-		// fontFamily: fonts.family.nunito.regular,
-		fontSize: 28,
-		fontWeight: "normal",
-		fontStyle: "normal",
-		letterSpacing: 0,
-		color: colors.black,
-		marginBottom: 22,
-		marginTop: 11,
-		marginLeft: 25
-	},
-	motto: {
-		// fontFamily: fonts.family.nunito.bold,
-		fontSize: 16,
-		fontWeight: "600",
-		fontStyle: "normal",
-		letterSpacing: 0,
-		color: colors.black,
-		marginTop: 30,
-		marginLeft: 25
-	},
-	next: {
-		// fontFamily: fonts.family.nunito.regular,
-		fontSize: 16,
-		fontWeight: "normal",
-		fontStyle: "normal",
-		letterSpacing: 0,
-		color: colors.white,
-		marginLeft: 25,
-		marginRight: 10
-	},
-	mainContent: {
-		marginStart: 32,
-		marginEnd: 32,
-		flex: 1,
-		paddingBottom: 50,
-		marginBottom: 50,
-	},
-	sitter: {
-		borderColor: colors.white, 
-		borderRadius: 20, 
-		width: "48%", 
-		height: 40, 
-		justifyContent: "center", 
-		alignItems: "center",
-	},
-	parent: {
-		borderColor: colors.white, 
-		borderRadius: 20, 
-		width: "48%", 
-		height: 40, 
-		justifyContent: "center", 
-		alignItems: "center",
-	},
-	headerLeft: {
-		marginStart: 32
-	}
-});
